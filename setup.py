eebffda94604272c17565debbac254cfe4f40f42
pullsss <100742515+pullsss@users.noreply.github.com>
2022-05-04 10:49:16 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='testoutput1946',
+  version='1.0.1',
+  description='oss-development class test output,
+  author='junhyuckan',
+  author_email='iloveujieun@naver.com',
+  url = "https://github.com/pullsss/testoutput",
+  
+  install_requires=['pytest'],
+  long_description = 'oss -development ...,
+  long_description_content_type = 'text/markdown',
+  classifiers={
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS independent",
+  ],
+ )
+  
